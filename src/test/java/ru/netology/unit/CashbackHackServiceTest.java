780b552c60d8ba0226e71c6f36be27a044836fbf
Dmitry Kravchenko <d.k.12.1972@gmail.com>
2021-12-08 14:39:37 +0500

    Падение теста при покупке 0.

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.unit;
 
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.ls.LSOutput;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,25 +12,40 @@ class CashbackHackServiceTest {
     @Test
     public void shouldMustAdd0() {
         assertEquals(1000, service.remain(0));
+
+        System.out.println();
+        System.out.println("      Nothing needs to be added");
     }
 
     @Test
     public void shouldMustAdd100() {
         assertEquals(900, service.remain(100));
+
+        System.out.println();
+        System.out.println("      To be added: 100");
     }
 
     @Test
     public void shouldMustAdd1To10000() {
         assertEquals(999, service.remain(1));
+
+        System.out.println();
+        System.out.println("      To be added: 1");
     }
 
     @Test
     public void shouldMustAdd999To1000() {
         assertEquals(1, service.remain(999));
+
+        System.out.println();
+        System.out.println("      To be added: 999");
     }
 
     @Test
     public void shouldMustAdd0To1000() {
-        assertEquals(1000, service.remain(0));
+        assertEquals(0, service.remain(1000));
+
+        System.out.println();
+        System.out.println("      To be added: 1000");
     }
 }
